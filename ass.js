e8ff7999febe72a83f1d99cc0da667530b18b9cd
fjkun_06 <fjkun06@gmail.com>
2021-11-03 08:26:32 +0100

    all good

--- ass.js
+++ ass.js
@@ -1,43 +1,27 @@
 $(function(){
     var file = $('#file');
-    var area = $('#ta');
-    var rcv = $('#txt');
-    var con = $('#container');
-    var all = document.getElementById('all');
     
-    
-    let hml = `
-    <div class="state" id="kool">
-        <div id="container">
-            <div id="one">
-                <img src="" alt="Coco" id="pic">
+
+
+   $('#btn').click(function(e){
+       e.preventDefault();
+         $('.kool').removeClass("state");
+         $('#all').prepend(
+         `<div class="state kool">
+            <div class="container">
+                <div class="one">
+                    <img src="Capture.PNG" alt="Coco" class="pic">
+                </div>
+                <div class="txt">${$('#texto').val()}</div>
             </div>
-            <div id="txt"></div>
         </div>
-    </div>
-    `;
-    $('#all').prepend(hml);
+        `);
 
-    //Previewing Image
-    file.change(function(x) {
-         var image = document.getElementById('pic');
-        image.src = URL.createObjectURL(x.target.files[0]);
-    });
-   
-    //Previewing Image
-    $('textarea').keyup(function(x) {
-        // `
-        $('#txt').text(`${x.target.value}`);
-        // `<p>${x.target.value}</p>`
-         console.log(x.target.value);
-         console.log(rcv.val())
-    });
-
+        file.change(function(x) {
+            var image = document.querySelector('img');
+           image.src = URL.createObjectURL(x.target.files[0]);
+       });
+   });
     
-    $('#btn').click(function(){
-        $('#kool').removeClass("state");
-        //Printing text from text Area to box
-        area.val("Enter your text here.");
-    });
-        
+
 });
